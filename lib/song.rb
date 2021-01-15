2569111379536cc2333c12b842e0e0df3195d194
Owen Cruse <ocruse7@gmail.com>
2021-01-15 02:07:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -44,10 +44,8 @@ class Song
     find_by_name(name) || create(name)
   end
 
-  # Song.new_from_filename("Thundercat - For Love I Come - dance.mp3")
   def self.new_from_filename(filename)
     parts = filename.split(" - ")
-    # binding.pry
     artist_name, song_name, genre_name = parts[0], parts[1], parts[2].gsub(".mp3", "")
 
     artist = Artist.find_or_create_by_name(artist_name)
